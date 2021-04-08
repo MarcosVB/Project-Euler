453f663afda4deaae00bf6874c87190947ca5389
MarcosVB <mvberte@hotmail.com>
2021-04-08 20:56:27 -0300

    Problem 18

--- Project Euler/src/Test.java
+++ Project Euler/src/Test.java
@@ -3,7 +3,7 @@ public class Test {
 
 	public static void main(String[] args) {
 		Problems p = new Problems();
-		// // //Problem 1
+		// //Problem 1
 		// System.out.println(p.MultiplesOf3and5(1000));
 		// //Problem 2
 		// System.out.println(p.EvenFibonacciNumbers(4000000));
@@ -44,10 +44,31 @@ public class Test {
 		// System.out.println(p.LongestCollatzSequence(1000000));
 		// //Problem 15
 		// System.out.println(p.LatticePaths(20, 0, 0));
-		//Problem 16
+		// //Problem 16
 		// System.out.println(p.PowerDigitSum(2, 1000));
-		//Problem 17
-		System.out.println(p.NumberLetterCounts(1000));
+		// //Problem 17
+		// System.out.println(p.NumberLetterCounts(1000));
+		//Problem 18
+		int[][] mat =   {{75},
+						{95, 64},
+						{17, 47, 82},
+						{18, 35, 87, 10},
+						{20, 04, 82, 47, 65},
+						{19, 01, 23, 75, 03, 34},
+						{88, 02, 77, 73, 07, 63, 67},
+						{99, 65, 04, 28, 06, 16, 70, 92},
+						{41, 41, 26, 56, 83, 40, 80, 70, 33},
+						{41, 48, 72, 33, 47, 32, 37, 16, 94, 29},
+						{53, 71, 44, 65, 25, 43, 91, 52, 97, 51, 14},
+						{70, 11, 33, 28, 77, 73, 17, 78, 39, 68, 17, 57},
+						{91, 71, 52, 38, 17, 14, 91, 43, 58, 50, 27, 29, 48},
+						{63, 66, 04, 68, 89, 53, 67, 30, 73, 16, 69, 87, 40, 31},
+						{04, 62, 98, 27, 23,  9, 70, 98, 73, 93, 38, 53, 60, 04, 23}};
+		System.out.println(p.MaximumPathSumI(mat, 0, 0, 0));
+						
+
+
+
 	}
 
 }
